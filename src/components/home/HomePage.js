0d0ab332e1e06a428208e9699afee434f7168280
Piotr Furmanek <info.pete85@gmail.com>
2024-09-04 12:05:26 +0100

    courses page

--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -1,13 +1,13 @@
 import React from "react";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const HomePage = () => (
   <div className="jumbotron">
-    <h1>Demo React Homepage</h1>
-    <p>React, Redux and React Router.</p>
-    <link to="about" className="btn btn-primary btn-lg">
+    <h1>Pluralsight Administration</h1>
+    <p>React, Redux and React Router for ultra-responsive web apps.</p>
+    <Link to="about" className="btn btn-primary btn-lg">
       Learn more
-    </link>
+    </Link>
   </div>
 );
 
