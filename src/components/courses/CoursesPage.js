9e6d658d0995a9bd1fc818cad16af7393ed4dad5
Piotr Furmanek <info.pete85@gmail.com>
2024-09-05 09:16:04 +0100

    courses page improvements

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -1,9 +1,7 @@
 import React from "react";
 
-class CoursesPage extends React.Component {
-  render() {
-    return <h2>Courses</h2>;
-  }
-}
+const CoursesPage = () => (
+  <h2>Courses</h2>
+);
 
 export default CoursesPage;
